1b8ca92c9eb672f8a93dfd6da519cae6267a4439
Sharad Chandra <sharadchandravns01@gmail.com>
2024-04-06 00:21:47 +0530

    ChangesOn 06/04

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ let phrases = [
   "deconstruct the lyrics of your favorite rap song", 
   "read the latest discussions on your favorite subreddits", 
   "calculate your future salary at Facebook", 
-  "think of smart responses to an argument you had last year"
+  "think of smart responses to an argument you had last year",
+  "read the news online and argue with people in the comments section",
+"listen to sad songs and relive the mistakes of your youth",
+"watch the trailer for a TV show and then the whole first season for bonus points",
+"consider taking a Data Science course"
 ];
 
 function getRandomElement(arr) {
@@ -21,4 +25,12 @@ let image = document.querySelector(".image");
 
 button.addEventListener("click",function(){
   phrase.textContent = getRandomElement(phrases);
+  if (RandomElement.length==40)
+  {
+    advice.style.fontSize = "33px";
+  }
+  else{
+    advice.style.fontSize = "42px";
+  }
+  phrase.textContent = randomElement;
 });
