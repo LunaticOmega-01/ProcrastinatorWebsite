4a2a32cc23fbd2aa3a9147e7869c2172fb319524
Sharad Chandra <sharadchandravns01@gmail.com>
2024-04-08 00:20:25 +0530

    ChangesOn08/04

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let phrases = [
-  "call your mom to ask how she's doing", 
-  "go through your old pictures on social media", 
-  "deconstruct the lyrics of your favorite rap song", 
-  "read the latest discussions on your favorite subreddits", 
-  "calculate your future salary at Facebook", 
-  "think of smart responses to an argument you had last year",
-  "read the news online and argue with people in the comments section",
-"listen to sad songs and relive the mistakes of your youth",
-"watch the trailer for a TV show and then the whole first season for bonus points",
-"consider taking a Data Science course"
+  { text: "call your mom to ask how she's doing", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png" }, 
+  { text: "go through your old pictures on social media", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"}, 
+  { text: "deconstruct the lyrics of your favorite rap song", image:"https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"}, 
+  { text: "read the latest discussions on your favorite subreddits", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"}, 
+  { text: "calculate your future salary at Facebook", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"}, 
+  { text: "think of smart responses to an argument you had last year", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"},
+  { text: "read the news online and argue with people in the comments section", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"},
+  { text: "listen to sad songs and relive the mistakes of your youth", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"},
+  { text: "watch the trailer for a TV show and then the whole first season for bonus points", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"},
+  { text: "consider taking a Data Science course", image: "https://practicum-content.s3.us-west-1.amazonaws.com/web-developer/procrastinator/images/moved_social.png"}
+  
 ];
 
 function getRandomElement(arr) {
@@ -21,16 +22,15 @@ let phrase = document.querySelector(".phrase");
 let advice = document.querySelector(".advice");
 let image = document.querySelector(".image");
 
+button.addEventListener("click", function () {
+  let randomElement = getRandomElement(phrases);
+  phrase.textContent = randomElement.text;
+  image.src = randomElement.image;
 
-
-button.addEventListener("click",function(){
-  phrase.textContent = getRandomElement(phrases);
-  if (RandomElement.length==40)
-  {
+  if (randomElement.text.length > 40) {
     advice.style.fontSize = "33px";
-  }
-  else{
+  } else {
     advice.style.fontSize = "42px";
   }
-  phrase.textContent = randomElement;
 });
+
